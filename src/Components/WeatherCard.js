04115746563022ec0df2f1adfd26ad0e4a324b73
Lily <licheng9312@gmail.com>
2021-09-13 19:46:09 +1000

    Added weather item placeholders

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  text-align: left;
+  margin: 16px;
+  padding: 16px 32px;
+  border-radius: 10px;
+`;
+
+const Item = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 16px;
+`;
 
 const WeatherCard = () => {
   return (
-    <div>
+    <Card className="card">
       <h3>Melbourne</h3>
       <h4>Feels like 10</h4>
-    </div>
+      <Item>
+        <p>Max</p>
+        <p>Min</p>
+        <p>Wind</p>
+        <p>Pressure</p>
+        <p>Humidity</p>
+      </Item>
+    </Card>
   );
 };
 
